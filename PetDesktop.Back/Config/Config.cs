c0ff975372e70c6024bd3bfbe8cdcfc50b332a11
Laura <lsp3114@gmail.com>
2026-08-20 18:44:36 +0200

    fix: properties order

--- PetDesktop.Back/Config/Config.cs
+++ PetDesktop.Back/Config/Config.cs
@@ -10,17 +10,23 @@ public static class Config
         .AddJsonFile("appsettings.json", false, true)
         .Build();
 
-    public static string SpriteSheetRoute { get; } = Path.Combine(
-        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "PetDesktop", SpriteSheetFolder);
-
     public static string SpriteSheetFolder { get; } =
         Settings.GetValue<string>("PetSettings:SpriteSheetSettings:SpriteSheetFolder") ?? "SpriteSheets";
 
-    public static string DefaultPetName { get; } = Settings.GetValue<string>("DefaultPet:Name") ?? "DefaultPet";
-    public static string DefaultSpriteSheetName { get; } = Settings.GetValue<string>("DefaultPet:InitialAnimation") ?? "Default_Animation";
+    public static string SpriteSheetRoute { get; } = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+        "PetDesktop",
+        SpriteSheetFolder);
 
-    public static string DefaultPetSpritesRoute { get; }= Settings.GetValue<string>("DefaultPet:Route") ??
-                                                          "avares://PetDesktop.App/Assets/DefaultPet";
+    public static string DefaultPetName { get; } =
+        Settings.GetValue<string>("DefaultPet:Name") ?? "DefaultPet";
+
+    public static string DefaultSpriteSheetName { get; } =
+        Settings.GetValue<string>("DefaultPet:InitialAnimation") ?? "Default_Animation";
+
+    public static string DefaultPetSpritesRoute { get; } =
+        Settings.GetValue<string>("DefaultPet:Route") ?? "avares://PetDesktop.App/Assets/DefaultPet";
+
     public static int DefaultPetFrameWidth { get; } = Settings.GetValue<int>("DefaultPet:FrameWidth");
     public static int DefaultPetFrameHeight { get; } = Settings.GetValue<int>("DefaultPet:FrameHeight");
 }
